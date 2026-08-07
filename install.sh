32bd59be2618d4b49a2c44000c5dad1ca0b4a720
yoshidaranimahato-collab <yoshidaranimahato@gmail.com>
2026-08-08 03:37:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-REPO="https://github.com/yoshidaranimahato-collab/SpyPanel.git"
-PANEL_DIR="$HOME/SpyPanel"
+REPO="https://github.com/yoshidaranimahato-collab/SpyPanel-2.git"
+PANEL_DIR="$HOME/SpyPanel-2"
 
 show_menu() {
     clear
